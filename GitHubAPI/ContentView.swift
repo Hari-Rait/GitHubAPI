31ec52cdac4699bc54e1ee1a77c50b303478953b
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2024-02-22 21:42:40 +0100

    Initial Commit

---
+++ GitHubAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubAPI
+//
+//  Created by Hari Rait on 22.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubAPI/GitHubAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAPIApp.swift
+//  GitHubAPI
+//
+//  Created by Hari Rait on 22.02.24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
